3e3c2ddd7eb3a4e210eab43f358c1a35d2073ce1
Alex Torscho <contact@alextorscho.com>
2022-03-12 12:10:03 +0100

    WIP

--- config/tm.php
+++ config/tm.php
@@ -2,6 +2,19 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Folders to Scan
+    |--------------------------------------------------------------------------
+    |
+    | Specify a list of folders to scan for translation strings.
+    | These paths must be relative to the root of your project.
+    |
+    */
 
+    'folders' => [
+        'app/Http/Controllers',
+        'resources/views',
+    ],
 
 ];
--- src/Commands/ScanFiles.php
+++ src/Commands/ScanFiles.php
@@ -3,6 +3,9 @@
 namespace ATStudio\TranslationManager\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Finder\SplFileInfo;
 
 class ScanFiles extends Command
 {
@@ -12,6 +15,26 @@ class ScanFiles extends Command
 
     public function handle()
     {
-        //
+        $foldersToScan = config('tm.folders');
+        $files = collect([]);
+        $strings = [];
+
+        foreach ($foldersToScan as $folder) {
+            $files->add(File::allFiles($folder));
+        }
+
+        /** @var Collection<SplFileInfo> $files */
+        $files = $files->flatten();
+
+        foreach ($files as $file) {
+            // dump($file->getContents());
+            preg_match_all('/__\([\'"](.+)[\'"]\)/', $file->getContents(), $matches);
+
+            if ($matches[1]) {
+                $strings = array_merge($strings, $matches[1]);
+            }
+        }
+
+        dd($strings);
     }
 }
