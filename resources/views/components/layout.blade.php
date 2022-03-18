220f6fe2b80ab8346a471b9ccf8a081ba1dd30d3
Alex Torscho <contact@alextorscho.com>
2022-03-19 00:40:20 +0100

    Added dark theme

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,13 +18,13 @@
     <link rel="stylesheet" href="{{ asset('vendor/translation-manager/assets/fontawesome/css/regular.min.css') }}" />
     @livewireStyles
 </head>
-<body class="bg-slate-100 pt-10 px-4">
+<body class="bg-slate-100 dark:bg-slate-800 pt-10 px-4">
 <div class="max-w-6xl mx-auto">
     <header class="flex items-center">
         <i class="fa-solid fa-language text-3xl text-teal-500 mr-4"></i>
-        <h1 class="text-slate-700 text-3xl font-raleway"><span class="font-bold">Translation</span> Manager</h1>
+        <h1 class="text-slate-700 dark:text-slate-300 text-3xl font-raleway"><span class="font-bold">Translation</span> Manager</h1>
 
-        <button class="ml-auto px-4 py-2 uppercase text-teal-50 inline-flex gap-3 items-center bg-teal-500 shadow-md rounded-full font-semibold text-sm tracking-wider transition duration-300 hover:bg-teal-600 group">
+        <button class="ml-auto px-4 py-2 uppercase text-teal-50 dark:text-teal-900 inline-flex gap-3 items-center bg-teal-500 shadow-md rounded-full font-semibold text-sm tracking-wider transition duration-300 hover:bg-teal-600 group">
             <i class="fa-solid fa-rotate transition duration-300 text-teal-700 group-hover:text-teal-800"></i>
             Scan Files
         </button>
@@ -33,23 +33,23 @@
     <div class="flex gap-5 mt-10">
         <aside class="w-72">
             <nav class="space-y-2">
-                <span class="px-3 py-2 rounded text-slate-800 flex items-center opacity-30">
+                <span class="px-3 py-2 rounded text-slate-800 dark:text-slate-50 flex items-center opacity-30">
                     English
 
                     <span class="ml-auto rounded bg-slate-200 px-2 py-0.5 text-slate-800 text-[11px] uppercase">Default</span>
                 </span>
-                <a class="px-3 py-2 rounded text-slate-800 flex items-center transition duration-300 bg-black/[.02]" href="#">
+                <a class="px-3 py-2 rounded text-slate-800 dark:text-slate-300 flex items-center transition duration-300 bg-black/[.02] dark:bg-white/[.03]" href="#">
                     <i class="fa-solid fa-circle text-teal-600 text-[8px] mr-2"></i>
 
                     French
                 </a>
-                <a class="px-3 py-2 rounded text-slate-800 flex items-center transition duration-300 hover:bg-black/[.03]" href="#">
+                <a class="px-3 py-2 rounded text-slate-800 dark:text-slate-300 flex items-center transition duration-300 hover:bg-black/[.03] dark:hover:bg-white/[.05]" href="#">
                     Russian
                 </a>
             </nav>
         </aside>
 
-        <main class="flex-1 bg-white rounded-md shadow">
+        <main class="flex-1 bg-white dark:bg-slate-700 rounded-md shadow">
             {{ $slot }}
         </main>
     </div>
--- resources/views/livewire/list-translations.blade.php
+++ resources/views/livewire/list-translations.blade.php
@@ -14,26 +14,26 @@
             </select>
         </div>
         <div class="hidden sm:block">
-            <div class="border-b border-slate-200">
+            <div class="border-b border-slate-200 dark:border-slate-800">
                 <nav class="-mb-px flex space-x-4 pl-8" aria-label="Tabs">
                     <a
                         href="?tab=all"
-                        class="{{ $this->isSelectedTab('all') ? 'border-teal-500 text-teal-600' : 'border-transparent text-slate-500 hover:text-slate-700 hover:border-slate-200' }} whitespace-nowrap flex p-4 border-b-2 font-medium text-sm"
+                        class="{{ $this->isSelectedTab('all') ? 'border-teal-500 text-teal-600 dark:border-teal-300 dark:text-teal-400' : 'border-transparent text-slate-500 hover:text-slate-700 hover:border-slate-200 dark:text-slate-200 dark:hover:text-slate-400 dark:hover:border-slate-400' }} whitespace-nowrap flex p-4 border-b-2 font-medium text-sm"
                         wire:click.prevent="goToTab('all')"
                     >
                         All
 
-                        <span class="{{ $this->isSelectedTab('all') ? 'bg-teal-100 text-teal-600' : 'bg-slate-100 text-slate-900' }} hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">{{ $allTranslations->count() }}</span>
+                        <span class="{{ $this->isSelectedTab('all') ? 'bg-teal-100 text-teal-600 dark:bg-teal-700 dark:text-teal-200' : 'bg-slate-100 text-slate-900 dark:bg-slate-500 dark:text-slate-300' }} hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">{{ $allTranslations->count() }}</span>
                     </a>
 
                     <a
                         href="?tab=missing"
-                        class="{{ $this->isSelectedTab('missing') ? 'border-teal-500 text-teal-600' : 'border-transparent text-slate-500 hover:text-slate-700 hover:border-slate-200' }} whitespace-nowrap flex p-4 border-b-2 font-medium text-sm"
+                        class="{{ $this->isSelectedTab('missing') ? 'border-teal-500 text-teal-600 dark:border-teal-300 dark:text-teal-400' : 'border-transparent text-slate-500 hover:text-slate-700 hover:border-slate-200 dark:text-slate-200 dark:hover:text-slate-400 dark:hover:border-slate-400' }} whitespace-nowrap flex p-4 border-b-2 font-medium text-sm"
                         wire:click.prevent="goToTab('missing')"
                     >
                         Missing
 
-                        <span class="{{ $this->isSelectedTab('missing') ? 'bg-teal-100 text-teal-600' : 'bg-slate-100 text-slate-900' }} hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">{{ $missingTranslations->count() }}</span>
+                        <span class="{{ $this->isSelectedTab('missing') ? 'bg-teal-100 text-teal-600 dark:bg-teal-700 dark:text-teal-200' : 'bg-slate-100 text-slate-900 dark:bg-slate-500 dark:text-slate-300' }} hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">{{ $missingTranslations->count() }}</span>
                     </a>
                 </nav>
             </div>
@@ -44,13 +44,13 @@
         @foreach ($translations as $default => $translation)
             <li class="group">
                 <label class="grid grid-cols-2 gap-3 items-center">
-                    <span class="bg-slate-50 group-first:rounded-t-md group-last:rounded-b-md px-4 py-3 text-[13px] tracking-wider">
+                    <span class="bg-slate-50 dark:bg-slate-600 text-slate-800 dark:text-slate-50 group-first:rounded-t-md group-last:rounded-b-md px-4 py-3 text-[13px] tracking-wider">
                         {{ $this->escapeDots($default) }}
                     </span>
                     <div>
                         <input
                             type="text"
-                            class="border border-slate-200 transition duration-300 flex w-full px-4 py-2 rounded focus:outline-none focus:border-teal-500 focus:ring-1 focus:ring-teal-500 shadow-inner text-sm font-light"
+                            class="border border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-400 transition duration-300 flex w-full px-4 py-2 rounded focus:outline-none focus:border-teal-500 dark:focus:border-teal-200 focus:ring-1 focus:ring-teal-500 dark:focus:ring-teal-200 shadow-inner text-sm font-light text-slate-800 dark:text-white"
                             placeholder="{{ $translation ? null : 'To be translated...' }}"
                             value="{{ $translation }}"
                             wire:model.defer="translations.{{ $default }}"
@@ -61,13 +61,13 @@
         @endforeach
 
         @if (count($translations) === 0)
-            <i class="font-italic">
+            <i class="font-italic text-slate-800 dark:text-slate-200">
                 {{ $currentTab === 'missing' ? 'All done!' : 'No translations have been extracted.' }}
             </i>
         @endif
     </ul>
 
-    <footer class="px-8 py-4 bg-slate-50 rounded-b-md">
+    <footer class="px-8 py-4 bg-slate-50 dark:bg-slate-600 rounded-b-md">
         <button type="submit" class="bg-teal-600 shadow-md text-white rounded uppercase text-sm px-4 py-3 tracking-widest transition duration-300 hover:bg-teal-700">
             <i class="fa-solid fa-floppy-disk text-teal-300 mr-2"></i>
             Save
